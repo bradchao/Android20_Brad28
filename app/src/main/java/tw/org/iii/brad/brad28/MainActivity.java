1ebefcb01f57c9f8f27ddf557a28e28bde07033d
user <brad@brad.tw>
2020-04-12 10:43:10 +0800

    1

---
+++ app/src/main/java/tw/org/iii/brad/brad28/MainActivity.java
@@ -0,0 +1,25 @@
+package tw.org.iii.brad.brad28;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+        DatePickerDialog dialog = new DatePickerDialog(this,
+                DatePickerDialog.THEME_DEVICE_DEFAULT_DARK, null,
+                2020,4-1,12);
+        dialog.show();
+
+
+    }
+}
